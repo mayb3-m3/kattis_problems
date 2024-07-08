9e21da77b8e783e963069ad9fd0599d01ab7748e
mayb3-m3 <iamashik423@gmail.com>
2024-07-08 06:27:07 +0600

    Create mirror.cpp

---
+++ mirror.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+#define imax INT_MAX
+#define llmax INT64_MAX
+#define ld long double
+#define ll long long
+#define ull unsigned long long
+#define pub push_back
+#define puf push_front
+#define pob pop_back
+#define pof pop_front
+#define ff first
+#define ss second
+#define mkpr make_pair
+#define stpr(x) setprecision(x)
+#define set(n,x) n |= (1 << x)
+#define unset(n,x) n &= ~(1 << x)
+#define flip(n,x) n ^= (1 << x)
+#define check(n, x) (n & (1 << x)) != 0
+#define sorted(x) sort(x.begin() , x.end())
+#define nsorted(x,n) sort(x.begin(), x.begin()+n)
+#define rsorted(x) sort(x.rbegin() , x.rend())
+#define nrsorted(x,n) sort(x.rbegin() , x.rbegin()+n)
+#define rev(x) reverse(x.begin(), x.end())
+#define maxel(x) *max_element(x.begin(), x.end())
+#define minel(x) *min_element(x.begin(), x.end())
+#define maxell(x) max_element(x.begin(), x.end())
+#define minell(x) min_element(x.begin(), x.end())
+#define next_p(x) next_permutation(x.begin(), x.end())
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define mod 1000000007
+#define pie 3.141592653589793238
+#define e_ 2.718281828459045235 
+
+using namespace std;
+
+void solve()
+{
+    int tt; cin >> tt;
+    for(int i = 0; i < tt; i++){
+        int r, c;
+        cin >> r >> c;
+        vector<string> vtr(r);
+        for(int i = 0; i < r; i++)
+            cin >> vtr[i];
+        cout << "Test " << i+1 << endl;
+        for(int i = 1; i <= r; i++){
+            for(int j = 1; j <= c; j++){
+                cout << vtr[r-i][c-j];
+            }
+            cout << endl;
+        }
+    }
+}
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve() ;
+}
