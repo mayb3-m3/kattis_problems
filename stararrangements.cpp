0892034b0ef125a08644ef47fb2eae863085d554
Ashikur Rahman <iamashik423@gmail.com>
2024-09-16 01:29:21 +0600

    Create stararrangements.cpp

---
+++ stararrangements.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+#define imax INT_MAX
+#define llmax INT64_MAX
+#define ld long double
+#define ll long long
+#define ull unsigned long long
+#define pub push_back
+#define puf push_front
+#define pob pop_back
+#define pof pop_front
+#define ff first
+#define ss second
+#define si size
+#define bc back
+#define ft front
+#define mkpr make_pair
+#define stpr(x) setprecision(x)
+#define set(n,x) n |= (1 << x)
+#define unset(n,x) n &= ~(1 << x)
+#define flip(n,x) n ^= (1 << x)
+#define check(n, x) (n & (1 << x)) != 0
+#define sorted(x) sort(x.begin() , x.end())
+#define rsorted(x) sort(x.rbegin() , x.rend())
+#define rev(x) reverse(x.begin(), x.end())
+#define maxel(x) *max_element(x.begin(), x.end())
+#define minel(x) *min_element(x.begin(), x.end())
+#define maxeld(x) max_element(x.begin(), x.end())
+#define mineld(x) min_element(x.begin(), x.end())
+#define next_p(x) next_permutation(x.begin(), x.end())
+#define _rand(x,y) min(x,y)+rand()%(abs(y-x)+1)
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pss pair<string, string>
+#define psi pair<string, int>
+#define pis pair<int, string>
+#define pque priority_queue
+#define umap unordered_map
+#define uset unordered_set
+#define ummap unordered_multimap
+#define umset unordered_multiset
+#define _mod 1000000007
+#define _pie 3.141592653589793238
+#define _e 2.718281828459045235
+
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    cout << n << ":\n";
+    for(int i = 2; i <= n; i++){
+        int a = i-1, b = i;
+        if(n % (i+a) == 0 || ((n-i) > 0 && (n-i) % (i+a) == 0))
+            cout << i << "," << a << endl;
+        if(n % (i+b) == 0 || ((n-i) > 0 && (n-i) % (i+b) == 0))
+            cout << i << "," << b << endl;
+    }
+}
+int main(){
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+}
