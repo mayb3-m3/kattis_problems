80f3e1d5522d6c59b5871fa82e34686fc02e55c6
Ashikur Rahman <iamashik423@gmail.com>
2024-09-17 21:02:09 +0600

    Create cardtrick2.cpp

---
+++ cardtrick2.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+#define imax INT_MAX
+#define llmax INT64_MAX
+#define ld long double
+#define ll long long
+#define ull unsigned long long
+#define pub push_back
+#define puf push_front
+#define pob pop_back
+#define pof pop_front
+#define ff first
+#define ss second
+#define si size
+#define bc back
+#define ft front
+#define mkpr make_pair
+#define stpr(x) setprecision(x)
+#define set(n,x) n |= (1 << x)
+#define unset(n,x) n &= ~(1 << x)
+#define flip(n,x) n ^= (1 << x)
+#define check(n, x) (n & (1 << x)) != 0
+#define sorted(x) sort(x.begin() , x.end())
+#define rsorted(x) sort(x.rbegin() , x.rend())
+#define rev(x) reverse(x.begin(), x.end())
+#define maxel(x) *max_element(x.begin(), x.end())
+#define minel(x) *min_element(x.begin(), x.end())
+#define maxeld(x) max_element(x.begin(), x.end())
+#define mineld(x) min_element(x.begin(), x.end())
+#define nextp(x) next_permutation(x.begin(), x.end())
+#define _rand(x,y) min(x,y)+rand()%(abs(y-x)+1)
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pss pair<string, string>
+#define psi pair<string, int>
+#define pis pair<int, string>
+#define pque priority_queue
+#define umap unordered_map
+#define uset unordered_set
+#define ummap unordered_multimap
+#define umset unordered_multiset
+#define _mod 1000000007
+#define _pie 3.141592653589793238
+#define _e 2.718281828459045235
+
+using namespace std;
+
+void solve(){
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        vector<int> vtr(n);
+        int pos = 0;
+        for(int i = 1; i <= n; i++){
+            int count = 0;
+            while(1){
+                if(vtr[pos] == 0)
+                    count++;
+                pos = (pos+1) % n;
+                if(vtr[pos] == 0 && count == i){
+                    vtr[pos] = i;
+                    break; 
+                }
+            }
+        }
+        for(int i : vtr)
+            cout << i << " ";
+        cout << endl;
+    }
+}
+int main(){
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+}
