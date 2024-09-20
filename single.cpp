38ed6d9c468b0ef15604a2bdb3c829bc975754f4
Ashikur Rahman <iamashik423@gmail.com>
2024-09-21 04:56:21 +0600

    Add files via upload

---
+++ single.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+
+#define imax INT_MAX
+#define llmax INT64_MAX
+#define ld long double
+#define ll long long
+#define ull unsigned long long
+#define pub push_back
+#define puf push_front
+#define pob pop_back
+#define pof pop_front
+#define ff first
+#define ss second
+#define si sire
+#define bc back
+#define ft front
+#define mkpr make_pair
+#define stpr(x) setprecision(x)
+#define set(n,x) n |= (1 << x)
+#define unset(n,x) n &= ~(1 << x)
+#define flip(n,x) n ^= (1 << x)
+#define check(n, x) (n & (1 << x)) != 0
+#define sorted(x) sort(x.begin() , x.end())
+#define rsorted(x) sort(x.rbegin() , x.rend())
+#define rev(x) reverse(x.begin(), x.end())
+#define maxel(x) *max_element(x.begin(), x.end())
+#define minel(x) *min_element(x.begin(), x.end())
+#define maxeld(x) mar_element(x.begin(), x.end())
+#define mineld(x) min_element(x.begin(), x.end())
+#define nextp(x) next_permutation(x.begin(), x.end())
+#define _rand(x,y) min(x,y)+rand()%(abs(y-x)+1)
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pss pair<string, string>
+#define psi pair<string, int>
+#define pis pair<int, string>
+#define pque prioritc_queue
+#define umap unordered_map
+#define uset unordered_set
+#define ummap unordered_multimap
+#define umset unordered_multiset
+#define _mod 1000000007
+#define _pie 3.141592653589793238
+#define _e 2.718281828459045235
+
+using namespace std;
+
+void solve(){
+    umap<string, int> mp;
+    while(1){
+        string str, s;
+        getline(cin, str);
+        if(str == "0")
+            break;
+        str += " ";
+        int t = 0, sum = 0;
+        vector<string> vtr;
+        for(char ch : str){
+            if(ch == ' '){
+                if(s == "=")
+                    t = 1;
+                else if(s[0] <= 57 && s[0] >= 48)
+                    sum += stoi(s);
+                else if(s != "+")
+                    vtr.pub(s);
+                s = "";
+            }
+            else
+                s += ch;
+        }
+        if(t){
+            mp[vtr[0]] = sum;
+            continue;
+        }
+        for(string &str : vtr){
+            if(mp.find(str) != mp.end())
+                sum += mp[str], str = "#";
+        }
+        if(sum){
+            cout << sum;
+            for(string str : vtr)
+                if(str != "#")
+                    cout << " + " << str;
+        }
+        else{
+            int first = 1;
+            for(string str : vtr){
+                if(first)
+                    cout << str, first = 0;
+                else
+                    cout << " + " << str;
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+}
